001b9c56791959e6e2a33f7ca45660e30eb35ba3
cserhat <110173713+cserhat@users.noreply.github.com>
2022-10-12 16:01:17 +0200

    Creation de page index
    Je route tous les page

---
+++ Public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../App/Autoloader.php' ;
+
+App\Autoloader::register();
+if(isset($_GET['url'])){
+$router = new App\Routes\Router($_GET['url']);
+
+}
+else
+{
+    header('location:/home');
+}
+
+
+
+$router->get('', function(){require '../App/Views/home.php';});
+$router->get('/', function(){require '../App/Views/home.php';});
+
+$router->get('/home', function(){require '../App/Views/home.php';});
+$router->post('/home', function(){require '../App/Views/home.php';});
+
+$router->get('/quiz', function(){require '../App/Views/quiz.php';});
+$router->post('/quiz', function(){require '../App/Views/quiz.php';});
+
+$router->get('/score', function(){require '../App/Views/scoreboard.php';});
+
+$router->run();
+
